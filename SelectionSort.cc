69c70b1dec234c52dadc45c42bc65790878d0473
dim005 <dim005@ucr.edu>
2015-11-09 13:09:29 -0800

    added sorts and alg

---
+++ SelectionSort.cc
@@ -0,0 +1,27 @@
+#include "sortAlg.h"
+
+class SelectionSort::sortAlg() {
+    public:
+        void sort(Container *);
+};
+
+void sortAlg::sort (Container *c) {
+    int min;
+    int i, j, k;
+    
+    for (auto it = c.begin(); it != c.end(); it++) {
+        k++;
+    }
+
+    for (j = 0; j < k-1; j++) {
+        min = j;
+        for( i = j+1; i < k; i++) {
+            if(c.at(i) < c.at(min)) {
+                min = i;
+            }
+        }
+        if( min != j) {
+            swap(c.at(j), c.at(min));
+        }
+    }
+}
