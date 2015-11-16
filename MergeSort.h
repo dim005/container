8d762c62f256a90a90c646b39226326ff07974c9
dim005 <dim005@ucr.edu>
2015-11-16 13:20:34 -0800

    changed merge

--- MergeSort.h
+++ MergeSort.h
@@ -1,49 +1,52 @@
 #include "Container.h"
 #ifndef MERGESORTALGORITHM_H
 #define MERGESORTALGORITHM_H
+
 class MergeSortAlgorithm : public SortAlgorithm {
     public:
         void sort (Container*);
- //       Container * merge (Container*, Container*);
+        Container * merge (Container*, Container*);
 };
 
-// should this function return a Container or Container ptr? Also, what does
-// result equal when returned?
-/*Container* MergeSortAlgorithm::merge (Container* left, Container* right) {
+Container* MergeSortAlgorithm::merge (Container* left, Container* right) {
     Container* result; 
-    int j =0;
-    while (left->size() > j || right->size() > j) {
-        if (left->size() > j && right->size() > j) {
-            if (left->at(j) <= right->at(j)) {
-                result->insert(left->at(j));
-                j++;  
+    while (left->size() > 0 || right->size() > 0) {
+        if (left->size() > 0 && right->size() > 0) {
+            if (left->at(0) <= right->at(0)) {
+                result->insert(left->at(0));       
+                left->erase();  
             }
             else {
-                result->insert(right->at(j));
-                j++;
+                result->insert(right->at(0));
+                right->erase();
             }
-            if (left->size() > 0 ) {
-                for (int i = 0; i < left->size(); i++)
-                    result->insert(left->at(i));
-                break;
+        }
+        else if (left->size() > 0 ) {
+            for (int i = 0; i < left->size(); i++) {
+                result->insert(left->at(i));
             }
-            else if (right->size() > 0 ) {
-                for (int i = 0; i < right->size(); i++)
-                    result->insert(right->at(i));
-                break;
-            }
+            break;
+        }
+        else if (right->size() > 0 ) {
+            for (int i = 0; i < right->size(); i++) {
+                result->insert(right->at(i));
+            } 
+            break;
         } 
     }
+
     return result;
 }
-*/
+   
 void MergeSortAlgorithm::sort(Container* c) {
     if (c->size() <= 1)
-        return;   
-    
+        return c;   
+  
+    //int c_original_size = c->size();
+ 
     int middle = (c->size()+1)/2;
 
-    Container* left,* right;
+    Container* left,* right,* result;
 
     for (int i = 0; i < middle; i++) {
         left->insert(c->at(i));
@@ -52,34 +55,11 @@ void MergeSortAlgorithm::sort(Container* c) {
     for (int i = middle; i < c->size(); i++) {
         right->insert(c->at(i));
     }
-    int c_original_size = c.size();
-    int j = 0;
-    while (left->size() > 0 || right->size() > 0) {
-        if (left->size() > 0 && right->size() > 0) {
-            if (left->at(j) <= right->at(j)) {
-                c->insert(left->at(0));
-                left->erase();  
-            }
-            else {
-                c->insert(right->at(0));
-                right->erase();
-            }
-            if (left->size() > 0 ) {
-                for (int i = 0; i < left->size(); i++) {
-                    c->insert(left->at(i));
-                }
-                break;
-            }
-            else if (right->size() > 0 ) {
-                for (int i = 0; i < right->size(); i++) {
-                    c->insert(right->at(i));
-                }
-                break;
-            }
-        } 
-    }
-    while(c.size() != c_original_size) {
-        c.erase();
-    }
+    
+    left = sort(left);
+    right = sort(right);
+    result = merge(left, right);
+    c = result;
 }
+
 #endif
